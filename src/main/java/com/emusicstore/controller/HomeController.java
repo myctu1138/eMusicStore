868c8612b77e7968db33f6d1e064307a9ba6ec4c
Mustafa Ali <38395857+myctu1138@users.noreply.github.com>
2021-09-14 22:54:58 +0300

    Initial commit

---
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.emusicstore.controller;
+
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Le on 1/2/2016.
+ */
+
+@Controller
+public class HomeController {
+
+    private ProductDao productDao = new ProductDao();
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model) {
+        List<Product> products = productDao.getProductList();
+        model.addAttribute("products", products);
+
+        return "productList";
+    }
+
+    @RequestMapping("/productList/viewProduct/{productId}")
+    public String viewProduct(@PathVariable String productId, Model model) throws IOException{
+
+        Product product = productDao.getProductById(productId);
+        model.addAttribute(product);
+
+        return "viewProduct";
+    }
+}
